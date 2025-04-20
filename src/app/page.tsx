791156198e173af5b3a26e59d756d7154a27790a
Sri Hari Lakshmanan <sriharilakshmanan05@gmail.com>
2025-04-20 16:40:35 +0530

    feat: link home page to show snippet and new snippet pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import Link from 'next/link';
 import { db } from './db';
 
 export default async function Home() {
     const snippets = await db.snippet.findMany();
 
     const renderedSnippets = snippets.map((snippet) => {
-        return <div key={snippet.id}>{snippet.title}</div>;
+        return (
+            <Link
+                key={snippet.id}
+                href={`/snippets/${snippet.id}`}
+                className="flex items-center justify-between border p-2 rounded"
+            >
+                <div> {`${snippet.id}. ${snippet.title}`}</div>
+                <div>View</div>
+            </Link>
+        );
     });
 
-    return <div>{renderedSnippets}</div>;
+    return (
+        <div>
+            <div className="flex items-center justify-between">
+                <h2 className="text-xl font-bold">Snippets</h2>
+                <Link className="border p-2 rounded" href={`/snippets/new`}>
+                    New
+                </Link>
+            </div>
+            <div className="my-2 flex flex-col gap-2">{renderedSnippets}</div>
+        </div>
+    );
 }
